ec68b261e78a76050b6caf6bc981754eb7ce116b
GaneshVetri <131172896+GaneshVetri@users.noreply.github.com>
2023-08-02 21:48:57 +0530

    Initialization of website with login page
    Login page was uploaded along with separate pages for separate individuals

---
+++ login.js
@@ -0,0 +1,41 @@
+
+function myfunction(){
+debugger
+	//alert("Hi");
+try{
+	if(document.getElementById("myName").value != ""){
+		if (document.getElementById("myName").value == "Ganesh" && document.getElementById("mypwd").value == "123456"){
+			
+			document.getElementById("myName").value = "Ganesh";		
+			document.getElementById("mypwd").value = "123456";
+			window.location.href = "Mr Ganesh.html";
+			
+
+		}
+		else if(document.getElementById("myName").value == "Dharish" && document.getElementById("mypwd").value == "45678"){
+
+			// document.getElementById("myName").value = "Dharish";		
+			// document.getElementById("mypwd").value = "45678";
+			window.location.href = "Mr Dharish.html";
+
+		}
+		else if(document.getElementById("myName").value == "Vijay" && document.getElementById("mypwd").value == "98765"){
+
+			document.getElementById("myName").value = "Vijay";		
+			document.getElementById("mypwd").value = "98765";
+			window.location.href = "Mr Vijay.html";
+
+		}
+		else {
+		alert ("It is a Wrong Name");
+		}
+	}
+	else {
+		alert ("Please Enter Username and Password");
+	}
+}
+catch (err){
+	alert(err);
+}
+
+}
